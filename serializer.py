7b1468592bdef2a365659640dc09ef8aecbcfa4c
elvircn <elvircrn@gmail.com>
2017-12-30 23:12:58 +0100

    Shuffle before loading

--- serializer.py
+++ serializer.py
@@ -23,7 +23,6 @@ class Serializer:
 
         Serializer._datasets = None
 
-
     @staticmethod
     def shuffle_and_serialize(datasets):
         datasets[0] = helpers.unison_shuffled_copies(datasets[0][0], datasets[0][1])
@@ -35,9 +34,8 @@ class Serializer:
         np.save(data.CV_LABELS_FILE, datasets[2][1])
         Serializer._datasets = datasets
 
-
     @staticmethod
-    def load_npy_datasets():
+    def load_npy_datasets(shuffle_train=True):
         if Serializer._datasets is None:
             Serializer._datasets = [[np.load(data.TRAIN_FEATURES_FILE).astype(np.float32),
                                      np.load(data.TRAIN_LABELS_FILE)],
@@ -45,4 +43,7 @@ class Serializer:
                                      np.load(data.TEST_LABELS_FILE)],
                                     [np.load(data.CV_FEATURES_FILE).astype(np.float32),
                                      np.load(data.CV_LABELS_FILE)]]
+        if shuffle_train:
+            Serializer._datasets[0] = helpers.unison_shuffled_copies(Serializer._datasets[0][0],
+                                                                     Serializer._datasets[0][1])
         return Serializer._datasets
